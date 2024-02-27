eb6e5d61fc5d53d74b124df9806d3bcee602ec74
Kshitij Kumar <kumarkshitij171@gmail.com>
2024-02-28 00:23:00 +0530

    Update ProjectSection component styling

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -3,7 +3,7 @@ import { ProjectCards } from "./ProjectCards";
 
 const ProjectSection = () => {
   return (
-    <div className="h-[90vh] pt-2">
+    <div className="min-h-[90vh] pt-2">
       <h3 className="mt-4 ml-2 text-3xl md:text-5xl font-bold  text-neutral-300 text-center">
         Projects
       </h3>
